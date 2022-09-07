43d1cd63bdf546132da95838ec632bd64d1c3eb7
FramFriordos <p72gupof@gmail.com>
2022-09-08 00:05:49 +0200

    commit inicial

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+const Pokemon = ({ pokemon }) => {
+  //filter(x => x) filtra los vacíos
+  //pop() retorna el ultimo elemento de un array
+  const id = pokemon.url.split('/').filter(x => x).pop()
+  return (
+    <li><Link href={`pokemones/${id}`}>{pokemon.name}</Link></li>
+  )
+}
+
+export default function Pokemones({ pokemones }) {
+  console.log(pokemones)
+  return (
+    <div>
+      <p>Pokemones</p>
+      <ul>
+        {pokemones.map(pokemon => <Pokemon pokemon={pokemon} key={pokemon.name} />)}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  const data = await response.json()
+
+  return {
+    props: { pokemones: data.results }
+  }
+}
---
+++ pages/pokemones/index.js
@@ -0,0 +1,7 @@
+const Index = () => {
+    return (
+        <p>soy un Index</p>
+    )
+}
+
+export default Index
